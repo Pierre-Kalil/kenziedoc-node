03582aadf3dfe6e5ac47b3799c4c78de7a2822da
Pierre Kalil <pierrekalil1@gmail.com>
2022-03-08 11:52:41 -0300

    feat: add function to auto create diretory tmp

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,22 @@ import { handleError } from "./middlewares/errors.middlewares";
 import swaggerUiExpress from "swagger-ui-express";
 import swaggerDocument from "./swagger.js";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+if (!path.join(__dirname, "utils/tmp")) {
+  fs.mkdir(path.join(__dirname, "utils/tmp"), (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("Directory created successfully!");
+  });
+}
 
 app.use(
   "/docs",
